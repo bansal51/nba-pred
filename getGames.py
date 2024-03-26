37a6fced61b8ee3dfd40913331f8732a29377e2b
unknown <yashyog2012@gmail.com>
2024-03-25 20:26:58 -0500

    added new file that gets past matches

---
+++ getGames.py
@@ -0,0 +1,39 @@
+# getGames.py - Get games on a day from a past season or present season
+
+from nba_api.stats.endpoints import leaguegamelog, scoreboard
+from teams import getTeamDict
+
+# This functions gets all games from a past specified date
+# Return value is a list: index 0 is a dict that holds the matchup and index 1 holds the result of the games
+# date format: mm/dd/yyyy; season format: yyyy-yy
+def pastMatches(date, season):
+    # get the list of the teams and their matchups on a specific date
+    matches = leaguegamelog.LeagueGameLog(season=season, league_id='00', season_type_all_star='Regular Season', date_from_nullable=date, date_to_nullable=date)
+    matchesDict = matches.get_normalized_dict()
+    matchList = matchesDict["LeagueGameLog"]
+    
+    # Traverse the matchlist and get the matchup and victor of the match
+    # We are stepping by 2 because the match appears twice
+    victorList = []
+    homeAwayDict = {}
+    for i in range(0, len(matchList), 2):
+        # determining whether or not the team is home or away
+        # @ in matchup indicates that the current team is away
+        if '@' in matchList[i]["MATCHUP"]:
+            awayTeam = matchList[i]["TEAM_NAME"]
+            homeTeam = matchList[i + 1]["TEAM_NAME"]
+            
+            victorList.append(matchList[i + 1]["WL"]) # Appends if the home team won or lost
+        else:
+            awayTeam = matchList[i + 1]["TEAM_NAME"]
+            homeTeam = matchList[i]["TEAM_NAME"]
+
+            victorList.append(matchList[i]["WL"])
+        
+        homeAwayDict.update({homeTeam: awayTeam}) # Adds the current game to the list of all games for the day
+
+    matchupResults = [homeAwayDict, victorList]
+    return matchupResults
+
+
+print(pastMatches("12/25/22", "2022-23"))
